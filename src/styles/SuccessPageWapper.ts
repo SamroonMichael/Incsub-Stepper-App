948593c008add82af04e6db133689108827a07f2
Samroon Michael <samroonmichael@gmail.com>
2022-05-16 10:50:10 +0500

    Rename bg image name

--- src/styles/SuccessPageWapper.ts
+++ src/styles/SuccessPageWapper.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import BG from '../img/background.jpeg';
+import BG from '../img/background-img.jpeg';
 
 export const SignUpPageWapper = styled.div`
     display: grid;
